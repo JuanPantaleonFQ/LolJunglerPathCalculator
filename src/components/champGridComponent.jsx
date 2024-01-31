b8a06328eee4a8ca994058d82e3b5b8fcd4b6825
Jaime Reyes <126992552+jaimereyess@users.noreply.github.com>
2024-01-31 23:52:37 +0000

    new data fetching

--- src/components/champGridComponent.jsx
+++ src/components/champGridComponent.jsx
@@ -1,23 +1,31 @@
-import Card from './card'
+import { useState, useEffect } from 'react';
+import Card from './Card';
 
-const getChamps = async () => {
-    const champs = await fetch('https://ddragon.leagueoflegends.com/cdn/12.4.1/data/en_US/champion.json')
-        .then(response => response.json())
-    
-    return champs
-}
+const ChampGridComponent = () => {
+  const [champs, setChamps] = useState([]);
 
-const ChampGridComponent =  async () => {
-   const campeon = await getChamps()
-   console.log(campeon) 
-    return(
-        <div className="mt-8 flex mx-auto px-5"> 
-              
-            {/* <Card name="brand" description="champ 1"/>
-            <Card name="lulu" description="champ 2"/>
-            <Card name="bardo" description="champ 3"/>  */}
-        </div>
+  useEffect(() => {
+    const getChamps = async () => {
+      try {
+        const response = await fetch('https://ddragon.leagueoflegends.com/cdn/12.4.1/data/en_US/champion.json');
+        const data = await response.json();
+        const championList = Object.values(data.data);
+        setChamps(championList);
+      } catch (error) {
+        console.error('Error fetching champions:', error);
+      }
+    };
+
+    getChamps();
+  }, []); 
+
+  return (
+    <div className="mt-8 flex mx-auto px-5 flex-wrap gap-5">
+      {champs.map((champ) => (
+        <Card key={champ.id} name={champ.name} description={`Champ ${champ.id}`} />
+      ))}
+    </div>
     )
 }
 
-export default ChampGridComponent
+export default ChampGridComponent;
