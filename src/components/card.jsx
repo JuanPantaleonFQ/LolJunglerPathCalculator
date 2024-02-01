405d33e2e58b7d378a97b49b46e3ee869b26b2ba
Juan Femenia Quevedo <53376393+JuanPantaleonFQ@users.noreply.github.com>
2024-02-01 13:33:28 +0000

    img implemented

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -3,10 +3,10 @@ const NameReturner = ({prop}) => <p className="my-4 pl-4 font-bold text-gray-500
 const DescriptionReturner = ({prop}) =>  <p className="mb-4 ml-4 text-xl font-semibold text-gray-800">{prop}</p>
 
 
-const Card = ({name,description}) => {
+const Card = ({name,description, img}) => {
     return(
-        <div className="max-w-60 cursor-pointer rounded-lg bg-white p-2 shadow duration-150 hover:scale-105 hover:shadow-md">
-            <img className="w-full rounded-lg object-cover object-center" src="https://images.unsplash.com/photo-1511556532299-8f662fc26c06?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="product" />
+        <div className="w-1/6 cursor-pointer rounded-lg bg-white p-2 shadow duration-150 hover:scale-105 hover:shadow-md">
+            <img className="w-full rounded-lg object-cover object-center" src={img} alt="product" />
             <NameReturner prop={name}/>
             <DescriptionReturner prop={description}/>
       </div>
--- src/components/champGridComponent.jsx
+++ src/components/champGridComponent.jsx
@@ -3,11 +3,12 @@ import Card from './Card';
 
 const ChampGridComponent = () => {
   const [champs, setChamps] = useState([]);
+  const [champImg, setChampImg] = useState([])
 
   useEffect(() => {
     const getChamps = async () => {
       try {
-        const response = await fetch('https://ddragon.leagueoflegends.com/cdn/12.4.1/data/en_US/champion.json');
+        const response = await fetch('https://ddragon.leagueoflegends.com/cdn/12.6.1/data/en_US/champion.json');
         const data = await response.json();
         const championList = Object.values(data.data);
         setChamps(championList);
@@ -17,12 +18,27 @@ const ChampGridComponent = () => {
     };
 
     getChamps();
+    
   }, []); 
+  console.log(champs)
+  // useEffect(()=>{
+  //   const getImgApi = async () => {
+  //     try {
+  //       const res = await fetch('https://ddragon.leagueoflegends.com/cdn/12.6.1/data/en_US/champion.json')
+  //       const data = await res.json()
+  //       const champImages = Object.values(data.data.img.full)
+  //       setChampImg(champImages)
+  //     } catch (error) {
+  //       console.log('Erro fetching images:', error);
+  //     }
+  //   }
+  //   getImgApi()
+  // }, [])
 
   return (
-    <div className="mt-8 flex mx-auto px-5 flex-wrap gap-5">
+    <div className="mt-8 flex mx-auto px-5 flex-wrap gap-5 ml-24">
       {champs.map((champ) => (
-        <Card key={champ.id} name={champ.name} description={`Champ ${champ.id}`} />
+        <Card key={champ.id} name={champ.name} description={champ.title} img={`https://ddragon.leagueoflegends.com/cdn/12.6.1/img/champion/${champ.image.full}`}  />
       ))}
     </div>
     )
