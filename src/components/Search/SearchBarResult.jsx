f4d0a401440959bca0a90bc301c6864de26c112c
Juan Femenia Quevedo <53376393+JuanPantaleonFQ@users.noreply.github.com>
2024-02-04 14:00:34 +0000

    search bar css and animation

--- src/components/Search/SearchBarResult.jsx
+++ src/components/Search/SearchBarResult.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import ResultComponent  from './resultComponent';
+import './css/scrollbar.css'
 
 const SearchBarResult = ({ newFilter , props }) => {
-    console.log(newFilter)
+    
     if (newFilter !== '') {
         return (    
-            <div className='w-64 flex flex-col items-center justify-end  text-black '> 
+            <div className='w-64 max-h-40 flex flex-col items-center justify-en  text-black overflow-auto custom-scrollbar'> 
                {props.map(campeon => (<ResultComponent key={campeon.id}prop={campeon.name}/>))}
             </div>           
         )
--- src/components/Search/searchBar.jsx
+++ src/components/Search/searchBar.jsx
@@ -26,7 +26,7 @@ const SearchBar = ({champs}) =>{
                          <svg className="w-5 h-5 text-gray-500 dark:text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path  d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
                      </div>
     
-                     <input onChange={handleInputChange}type="search" id="default-search" className="block p-4 pl-10 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" value={newFilter} placeholder="Search"/>
+                     <input onChange={handleInputChange} type="search" id="default-search" autoComplete="off" className="block p-4 pl-10 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" value={newFilter} placeholder="Search"/>
                      <button type="submit" className="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
                  </div>
              </form>        
